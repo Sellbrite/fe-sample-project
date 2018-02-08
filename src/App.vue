4c2379387d3500ceb8d3af2c5e3ec790d3401e7f
Daniel Romero <romerodaniel.k90@gmail.com>
2018-02-08 15:33:39 -0800

    flexbox solutions, nav structured

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,50 @@
 <template>
   <div id="app">
     <Nav/>
-    <Products/>
+    <Products class="main-component"/>
   </div>
 </template>
 
 <script>
-import Products from './components/products.vue'
-import Nav from './components/nav.vue'
+import Products from "./components/products.vue";
+import Nav from "./components/nav.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Products,
-    Nav,
+    Nav
   }
-}
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+  min-height: 100vh;
+  flex-direction: column;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-size: 18px;
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+#app a,
+#app p {
+  margin: 1rem 0;
+  display: block;
+}
+#app .component {
+  max-width: 1200px;
+  margin: auto;
+}
+#app .main-component {
+  flex: 1;
 }
 </style>
--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,16 +1,54 @@
 <template>
-  <div class="">
-    <p>
-      Navigation 
-    </p>
-  </div>
+  <nav>
+    <div class="component">
+      <ul class="nav-link-list">
+        <li class="nav-link-item">
+          <p>Cart.ly</p>
+        </li>
+        <li class="nav-link-item">
+          <a>Shop</a>
+        </li>
+        <li class="nav-link-item floated-list-item">
+          <p>
+            Your Cart
+            <span>{{counter}}</span>
+          </p> 
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-  name: "Nav"
+  name: "Nav",
+  data: () => {
+    return {
+      counter: 0
+    };
+  }
 };
 </script>
 
 <style scoped>
+nav {
+  background-color: rgb(26, 0, 217);
+}
+p,
+a {
+  color: white;
+}
+.nav-link-list {
+  display: flex;
+  list-style-type: none;
+  margin: 0;
+  padding: 0 2rem;
+}
+
+.nav-link-item {
+  margin: 0 0.5rem;
+}
+.nav-link-item.floated-list-item {
+  margin-left: auto;
+}
 </style>
