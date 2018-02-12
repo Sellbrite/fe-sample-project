598728d4b97037c67db3e02cdc8d84b3587097c6
Justin Caryk <cary9983@gmail.com>
2018-02-12 14:44:48 -0800

    added .env

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // const fs = require('fs')
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const router = require('./app/routes')
 
-//set port
-let port = 8080
+// initialize dotenv
+dotenv.config()
+
+// set our port
+const port = process.env.PORT || 8080
 
 // parse application/json
 app.use(bodyParser.json())
